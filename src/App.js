40f4208daf6dee84bcee0be304bef382745d25f8
Dennis Kimathi <kimathidennis38@gmail.com>
2022-06-24 01:18:18 +0300

    file and folder structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Catalog, Credits, Contribute, Forum, Home, Login, Messages, Searches, Signup } from './mainroutes';
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App;
---
+++ src/mainroutes/index.js
@@ -0,0 +1,15 @@
+export { default as Catalog } from './Catalog/Catalog';
+export { default as Contribute } from './Contribute/Contribute'
+export { default as Credits} from './Credits/Credits'
+export { default as Forum } from './Forum/Forum'
+export { default as Home } from './Home/Home'
+export { default as Login } from './Login/Login'
+export { default as Messages } from './Messages/Messages'
+export { default as Searches } from './Searches/Searches'
+export { default as Signup } from './Signup/Signup'
+export { default as Tools } from './Tools/Tools'
+
+
+
+
+
